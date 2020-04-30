c47b8b7efd1f1923d56b3ecf76650bf46ef03f4b
Tino Martínez <43123591+tinomtzlpz@users.noreply.github.com>
2020-04-30 10:56:55 -0500

    modificado para escribir la estructura a un archivo binario

--- EstructuraPersona.cpp
+++ EstructuraPersona.cpp
@@ -7,7 +7,7 @@ typedef struct persona
 	unsigned long int id_persona;
 	char nombre[100];
 	char sexo;
-	char dirección[100], religion[50], escolaridad[20];
+	char direccion[100], religion[50], escolaridad[20];
 	unsigned short int edad;
 } Persona;
 
@@ -16,13 +16,39 @@ typedef struct persona
 
 int main()
 {
+    Persona array_de_personas[10];
 	Persona una_persona;
+    char c;
+    FILE *aarchivo;
 
-	int a, b;
-	char nombre_completo[100];
+	//int a, b;
+	//char nombre_completo[100];
+    array_de_personas[0].id_persona = 1;
 	printf("Introduzca su nombre completo:\n");
-	fgets(nombre_completo, 99,stdin);
-	printf("%s", nombre_completo);
+	fgets(array_de_personas[0].nombre, 99,stdin);
+    printf("Introduzca el sexo de la persona [H/M]:\n");
+    array_de_personas[0].sexo = getc(stdin);
+    while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+    printf("Introduzca la direccion:\n");
+    fgets(array_de_personas[0].direccion, 99,stdin);
+    printf("Introduzca la religion:\n");
+    fgets(array_de_personas[0].religion, 49,stdin);
+    printf("Introduzca la escolaridad:\n");
+    fgets(array_de_personas[0].escolaridad, 19,stdin);
+    printf("Introduzca la edad:\n");
+    scanf("%hu", &array_de_personas[0].edad);
+    while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+
+    aarchivo = fopen("miarchivo.bin", "w");
+    if(aarchivo == NULL)
+    {
+        printf("Error al abrir archivo.\n");
+        return(1);
+    }
+
+    fwrite(array_de_personas, sizeof(Persona), 10, aarchivo);
+
+    fclose(aarchivo);
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
